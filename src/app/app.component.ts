498ffae4b2ced91546451885f1126900bd0905ed
DESKTOP-LJ8120N\danie <danielrandell@outlook.com>
2019-10-23 17:57:26 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'test-app';
+  streamForm: FormGroup;
+  validName = new RegExp(/^[A-Za-z0-9\s]+$/);
+
+  ngOnInit(): void {
+    this.streamForm = new FormGroup({
+      name: new FormControl('dan', [ Validators.required, Validators.maxLength(9), Validators.pattern(this.validName) ]),
+    });
+
+    this.valueChange();
+  }
+
+  valueChange(): void {
+    this.streamForm.get('name').valueChanges.subscribe(
+      (value: any) => {
+        // Define values
+        let streamName = value;
+        // Create invalid regexp
+        const invalid = new RegExp(/[A-Za-z0-9 ]+/g);
+        // Get any invalid characters in our string
+        const invalidChar = streamName.replace(invalid, '');
+
+        console.log('value = ', value);
+        console.log('invalid char = ', invalidChar);
+        console.log('test = ', this.validName.test(value));
+
+        if (!this.validName.test(streamName)) {
+          this.streamForm.get('name').setErrors( { validation_fail: true } );
+
+          streamName = streamName.replace(invalidChar, '');
+
+          if (streamName !== value) {
+            this.streamForm.get('name').setValue(streamName);
+          }
+        }
+      }
+    );
+  }
 }
